58d7df4af5388d6753b8d7ff66ae703ab5198f20
aliraloarca <andreamaliralo@gmail.com>
2023-10-18 09:57:00 +0200

    fix issues

--- 03_debug.py
+++ 03_debug.py
@@ -4,10 +4,7 @@
 # Prova -> Laura dopo cancellazione
 
 print('ciao')
-<<<<<<< HEAD
-=======
 
->>>>>>> 340f21a (change names py pandas)
 u = 1
 v = 4
 p = 4
@@ -15,13 +12,10 @@ u = 9
 #%%
 from utils import uv2mag
 Uw = uv2mag(u, v)
-<<<<<<< HEAD
 #%%
 # %%
-=======
 
 
->>>>>>> 340f21a (change names py pandas)
 import numpy as np
 u2 = np.arange(1, 10)
 v2 = np.arange(5, 14)
@@ -30,12 +24,9 @@ Uw2 = uv2mag(u2, v2)
 
 ### prova Patti
 
-<<<<<<< HEAD
 # Questa è una prova di Edo
 
 ##ciao sono Antonio
-=======
 # Questa è una prova di Edo ;-)
 # prova Fede
 # %%
->>>>>>> 340f21a (change names py pandas)
